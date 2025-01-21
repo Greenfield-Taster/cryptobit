de1cc19c13d504d85ddd84ab784a9269ea145b9c
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2025-01-21 15:32:46 +0200

    Add footer and it`s styles

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+// Footer.jsx
+import React from "react";
+import "../../scss/footer.scss";
+import logo2 from '../../assets/images/logo2.png'
+import bybitLogo from '../../assets/images/Bybit-Logo.png'
+import privatLogo from '../../assets/images/Privat24_Logo.png'
+import binanceLogo from '../../assets/images/binance-logo.png'
+import monoLogo from '../../assets/images/monobank-logo.png'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__main">
+          <div className="footer__logo"><img src={logo2} alt="logo"/></div>
+          <div className="footer__links">
+            <a href="/privacy">Privacy Policy</a>
+            <a href="/terms">Terms & Conditions</a>
+          </div>
+        </div>
 
-export default Footer
+        <div className="footer__bottom">
+          <div className="footer__copyright">
+            © 2025 Cryptobit. Developed by <span>GreenTeaTaster</span>
+          </div>
+          <div className="footer__payments">
+            <img src={bybitLogo} alt="ByBit" />
+            <img src={binanceLogo} alt="Binance" />
+            <img src={monoLogo} alt="Mono" />
+            <img src={privatLogo} alt="Privat" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
