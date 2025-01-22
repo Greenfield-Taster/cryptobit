2fb4612ca06c45ac1bd4e194d93b03423abaad69
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2025-01-22 15:46:01 +0200

    Add home section structure and it`s styles

--- src/components/Sections/HomeSection.jsx
+++ src/components/Sections/HomeSection.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import "../../scss/sections.scss";
+import mainImage from "../../assets/images/main-img.png";
 
 function HomeSection() {
   return (
     <section className="section">
       <div className="section__home">
-        <h2>Home</h2>
-        <p>This is the Home section.</p>
+        <div className="section__home-container">
+          <div className="section__home__content">
+            <div className="section__home__content-oval"></div>
+            <h1 className="section__home__title">
+              Ability to buy & cash
+              <br />
+              cryptocurrency
+            </h1>
+            <div className="section__home__cryptobit">
+              <span>Cryptobit</span>
+            </div>
+            <p className="section__home__description">
+              Cryptography, encryption process of transforming information
+              referred to as plaintext using done.
+            </p>
+          </div>
+
+          <div className="section__home__visuals">
+            <img src={mainImage} />
+          </div>
+        </div>
       </div>
     </section>
   );
