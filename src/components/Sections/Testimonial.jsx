f0916ffdf7aedfb9fe3f08ef36ba30cb8e1b20e0
Anastasiia Horbachova <77613323+Greenfield-Taster@users.noreply.github.com>
2025-03-27 15:29:58 +0200

    Add more testimonials and remove the navigation carousel

--- src/components/Sections/Testimonial.jsx
+++ src/components/Sections/Testimonial.jsx
@@ -3,18 +3,8 @@ import { useTranslation } from "react-i18next";
 import "../../scss/main.scss";
 import testimonialDataEn from "../../data/testimonials-en.json";
 import testimonialDataRu from "../../data/testimonials-ru.json";
-import team1 from "../../assets/images/testi/team1.png";
-import team2 from "../../assets/images/testi/team2.png";
-import team3 from "../../assets/images/testi/team3.png";
-import team4 from "../../assets/images/testi/team4.png";
+import userIcon from "../../assets/images/user-icon.png";
 import testiBack from "../..//assets/images/testi.png";
-
-const images = {
-  team1,
-  team2,
-  team3,
-  team4,
-};
 
 const testimonialsByLang = {
   en: testimonialDataEn,
@@ -23,7 +13,6 @@ const testimonialsByLang = {
 
 const Testimonial = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
-
   const { t, i18n } = useTranslation();
   const testimonials =
     testimonialsByLang[i18n.language]?.testimonials ||
@@ -32,7 +21,7 @@ const Testimonial = () => {
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentSlide((prev) => (prev + 1) % testimonials.length);
-    }, 10000);
+    }, 5000);
 
     return () => clearInterval(timer);
   }, [testimonials.length]);
@@ -66,6 +55,14 @@ const Testimonial = () => {
     return stars;
   };
 
+  const getPrevSlideIndex = (current) => {
+    return (current - 1 + testimonials.length) % testimonials.length;
+  };
+
+  const getNextSlideIndex = (current) => {
+    return (current + 1) % testimonials.length;
+  };
+
   return (
     <div className="section">
       <div className="section__testimonial">
@@ -92,51 +89,88 @@ const Testimonial = () => {
 
             <div className="testimonials__content">
               <div className="testimonials__slides">
-                {testimonials.map((testimonial, index) => (
-                  <div
-                    key={testimonial.id}
-                    className={`testimonial ${
-                      index === currentSlide ? "active" : ""
-                    }`}
-                  >
+                <div className="testimonial-carousel">
+                  <div className="testimonial prev">
                     <div className="testimonial__content">
                       <div className="testimonial__header">
                         <div className="testimonial__avatar">
-                          <img
-                            src={images[testimonial.image]}
-                            alt={testimonial.name}
-                          />
+                          <img src={userIcon} alt="userIcon" />
                         </div>
                         <div className="testimonial__info">
                           <h3 className="testimonial__name">
-                            {testimonial.name}
+                            {testimonials[getPrevSlideIndex(currentSlide)].name}
                             <span className="testimonial__rating">
-                              {renderStars(testimonial.rating)}
+                              {renderStars(
+                                testimonials[getPrevSlideIndex(currentSlide)]
+                                  .rating
+                              )}
                             </span>
                           </h3>
                           <p className="testimonial__role">
-                            {testimonial.role}
+                            {testimonials[getPrevSlideIndex(currentSlide)].role}
                           </p>
                         </div>
                         <span className="quote-icon">❝</span>
                       </div>
-                      <p className="testimonial__text">{testimonial.text}</p>
+                      <p className="testimonial__text">
+                        {testimonials[getPrevSlideIndex(currentSlide)].text}
+                      </p>
                     </div>
                   </div>
-                ))}
-              </div>
 
-              <div className="testimonials__navigation">
-                {testimonials.map((_, index) => (
-                  <button
-                    key={index}
-                    className={`nav-dot ${
-                      index === currentSlide ? "active" : ""
-                    }`}
-                    onClick={() => setCurrentSlide(index)}
-                    aria-label={`Go to slide ${index + 1}`}
-                  />
-                ))}
+                  <div className="testimonial active">
+                    <div className="testimonial__content">
+                      <div className="testimonial__header">
+                        <div className="testimonial__avatar">
+                          <img src={userIcon} alt="user icon" />
+                        </div>
+                        <div className="testimonial__info">
+                          <h3 className="testimonial__name">
+                            {testimonials[currentSlide].name}
+                            <span className="testimonial__rating">
+                              {renderStars(testimonials[currentSlide].rating)}
+                            </span>
+                          </h3>
+                          <p className="testimonial__role">
+                            {testimonials[currentSlide].role}
+                          </p>
+                        </div>
+                        <span className="quote-icon">❝</span>
+                      </div>
+                      <p className="testimonial__text">
+                        {testimonials[currentSlide].text}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="testimonial next">
+                    <div className="testimonial__content">
+                      <div className="testimonial__header">
+                        <div className="testimonial__avatar">
+                          <img src={userIcon} alt="user icon" />
+                        </div>
+                        <div className="testimonial__info">
+                          <h3 className="testimonial__name">
+                            {testimonials[getNextSlideIndex(currentSlide)].name}
+                            <span className="testimonial__rating">
+                              {renderStars(
+                                testimonials[getNextSlideIndex(currentSlide)]
+                                  .rating
+                              )}
+                            </span>
+                          </h3>
+                          <p className="testimonial__role">
+                            {testimonials[getNextSlideIndex(currentSlide)].role}
+                          </p>
+                        </div>
+                        <span className="quote-icon">❝</span>
+                      </div>
+                      <p className="testimonial__text">
+                        {testimonials[getNextSlideIndex(currentSlide)].text}
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
